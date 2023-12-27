3fdc654edfbd9d80d12e8def6bbd0dc97c2ced69
Ranjit Nepal <theonlyeminem@gmail.com>
2023-12-27 23:28:57 +0200

    format changed to take the inputs

--- main.c
+++ main.c
@@ -3,9 +3,26 @@
 #include<string.h>
 #include<stdlib.h>
 
+#define PROGRAM "wc"
+#define VERSION "truncet-wc-0.1"
+
+#define MAX_INPUT_LENGTH 256
+
+static int bytes_val, char_val, words_val, line_val, max_line_val;
+
 int main(int argc, char *argv[]);
 
+int find_params(char *file_name, int bytes_val, int char_val, int line_val, int words_val, int max_line_val);
+
 int show_help();
+
+int implement_characters(char *value);
+
+int implement_bytes(char *value);
+
+int implement_lines(char *value);
+
+int implement_words(char *value);
 
 
 int show_help(){
@@ -29,27 +46,58 @@ the following order: newline, word, character, byte, maximum line length.\n", st
 
 }
 
+int check_length_limit(char *value){
+  if (strlen(value) > MAX_INPUT_LENGTH)
+    return 0;
+  return 1;
+}
+
+
+int find_params(char *file_name, int bytes_val, int char_val, int line_val, int words_val, int max_line_val){
+  return 0;
+}
+
+int implement_characters(char *value){
+  printf("%s", value);
+  return 0;
+
+}
+
+int implement_bytes(char *value){
+  return 0;
+}
+
+int implement_lines(char *value){
+  return 0;
+}
+
+int implement_words(char *value){
+  return 0;
+} 
+
 int main(int argc, char *argv[]){
 
   int option_val;
 
   char *value = NULL;
 
+  char_val = bytes_val = words_val = line_val = max_line_val = 0;
+
   struct option long_options[] = {
     {
-      "bytes", required_argument, 0, 'c'
+      "bytes", no_argument, 0, 'c'
     },
     {
-      "chars", required_argument, 0, 'm'
+      "chars", no_argument, 0, 'm'
     },
     {
-      "lines", required_argument, 0, 'l'
+      "lines", no_argument, 0, 'l'
     },
     {
       "max-line-length", no_argument, 0, 'L'
     },
     {
-      "words", required_argument, 0, 'w'
+      "words", no_argument, 0, 'w'
     },
     {
       "version", no_argument, 0, 0
@@ -62,26 +110,32 @@ int main(int argc, char *argv[]){
 
   int option_index = 0;
 
-  while ( (option_val = getopt_long(argc, argv, "c:m:l:Lw:", long_options, &option_index)) != -1) {
+  while ( (option_val = getopt_long(argc, argv, "cmlLw", long_options, &option_index)) != -1) {
     switch (option_val){
       case('c'):
-        value = optarg;
-        printf ("%s\n", value);
+        char_val = 1;
         break;
       
       case ('m'):
-        value = optarg;
-        printf ("%s\n", value);
-        break;
-
-      case ('l'):
-        value = optarg;
-        show_help();
+        bytes_val = 1;
         break;
       
+      case ('l'):
+        line_val = 1;
+        break;
+
+      case ('L'):
+        max_line_val = 1;
+        break;
+
+      case ('w'):
+        words_val = 1;
+        break;
+
+     
       case 0:
         if (strcmp(long_options[option_index].name, "version") == 0){
-          fputs ("truncet-wc-0.1\n", stdout);
+          printf ("%s\n", VERSION);
         }
         else if (strcmp(long_options[option_index].name, "help") == 0){
           show_help();
@@ -90,12 +144,18 @@ int main(int argc, char *argv[]){
       
       case '?':
         fputs ("Try 'wc --help' for more information.\n", stdout);
-        break;
+        exit(1);
 
       default:
-        
-        exit(0);
+        exit(1);
     }
+
+  }
+
+
+  for (int i = optind; i < argc; i++){
+    printf("Filename %s\n", argv[i]);
+    find_params(argv[i], char_val, bytes_val, line_val, words_val, max_line_val);
   }
 
   return 0;
